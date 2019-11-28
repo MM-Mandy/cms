1e234cf093566efb60d7a48618ff85c510d87176
lizhu <963539536@qq.com>
2019-11-28 10:33:15 +0800

    完成链接管理功能

---
+++ src/main/java/com/briup/cms/service/ILinkService.java
@@ -0,0 +1,16 @@
+package com.briup.cms.service;
+
+import com.briup.cms.bean.Link;
+import com.briup.cms.exception.CustomerException;
+
+import java.util.List;
+
+public interface ILinkService {
+    void saveOrUpdate(Link link) throws CustomerException;
+
+    void deleteLink(int id) throws CustomerException;
+
+    Link findById(int id) throws CustomerException;
+
+    List<Link> fingAll() throws CustomerException;
+}
